cc963044c3b3b2045723f46cacba03fda5a28bc2
46112 <duf325@naver.com>
2025-03-16 17:57:12 +0900

    chore: Lint 스타일 체크를 위한 gradle 설정

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/StockAssistPlatformApplication.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/StockAssistPlatformApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StockAssistPlatformApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(StockAssistPlatformApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(StockAssistPlatformApplication.class, args);
+	}
 
 }
