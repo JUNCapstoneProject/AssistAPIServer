5d0b310f55176343ac79fed4108a7685fc654caf
46112 <duf325@naver.com>
2025-03-16 17:12:58 +0900

    feat: 초기 스프링부트 프로젝트 모듈 추가

---
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/StockAssistPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.help.stockassistplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAssistPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockAssistPlatformApplication.class, args);
+    }
+
+}
