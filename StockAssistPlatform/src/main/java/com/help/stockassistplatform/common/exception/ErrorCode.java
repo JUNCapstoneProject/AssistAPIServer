330f738c9e41483f0f0148a3de7aeb3a1cf5f466
46112 <duf325@naver.com>
2025-03-17 18:00:02 +0900

    feat: 비지니스 로직 에러 객체인 CustomException과 ErrorCode 추가

---
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/common/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.help.stockassistplatform.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+	USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다"),
+	EMAIL_ALREADY_EXISTS(409, "이미 존재하는 이메일입니다");
+
+	private final int status;
+	private final String message;
+
+	ErrorCode(final int status, final String message) {
+		this.status = status;
+		this.message = message;
+	}
+}
