fa3ed5750e683c71763a38f7dc86486591a0ee26
46112 <duf325@naver.com>
2025-03-17 20:57:09 +0900

    chore(test): H2 의존성 추가와 test DB환경 설정

--- StockAssistPlatform/src/test/java/com/help/stockassistplatform/StockAssistPlatformApplicationTests.java
+++ StockAssistPlatform/src/test/java/com/help/stockassistplatform/StockAssistPlatformApplicationTests.java
@@ -2,8 +2,10 @@ package com.help.stockassistplatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StockAssistPlatformApplicationTests {
 
 	@Test
