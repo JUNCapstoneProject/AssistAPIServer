0e926eea5b3f48df9d539a52ab2ab77480e7919e
46112 <duf325@naver.com>
2025-03-17 18:32:19 +0900

    feat(user): 설계에 맞추어 user엔티티에 필드 추가 - createdAt - type은 role로 필드 추가 - nickname은 추후 분리 예정

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/user/entity/User.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/user/entity/User.java
@@ -1,7 +1,15 @@
 package com.help.stockassistplatform.domain.user.entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -16,6 +24,7 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class User {
 	@Id
@@ -28,8 +37,16 @@ public class User {
 	@Column(nullable = false)
 	private String password;
 
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private UserRole role = UserRole.USER;
+
 	@Column(nullable = false, unique = true)
 	private String nickname;
+
+	@Column(nullable = false, updatable = false)
+	@CreatedDate
+	private LocalDateTime createdAt;
 
 	@Builder
 	public User(final String email, final String password, final String nickname) {
