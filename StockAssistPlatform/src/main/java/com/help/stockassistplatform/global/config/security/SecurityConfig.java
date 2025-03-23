946e3e9e1cc2158d0d9e0a81d46d50fb1a88132c
46112 <duf325@naver.com>
2025-03-23 18:24:10 +0900

    feat: 토큰 검증 필터를 필터 체인에 삽입

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/global/config/security/SecurityConfig.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/global/config/security/SecurityConfig.java
@@ -9,13 +9,31 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
 
+import com.help.stockassistplatform.global.jwt.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+	// TODO: CSRF 토큰 사용 시 주석 해제
+	// @Bean
+	// public CsrfTokenRepository csrfTokenRepository() {
+	// 	HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+	// 	repository.setHeaderName("X-XSRF-TOKEN");
+	// 	return repository;
+	// }
 
 	@Bean
 	public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+		// http.csrf(csrf -> csrf.csrfTokenRepository(csrfTokenRepository())
+		// 	.ignoringRequestMatchers("/login")
+		// )
 		http.csrf(AbstractHttpConfigurer::disable);
 		http.sessionManagement(session -> session
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -23,7 +41,9 @@ public class SecurityConfig {
 		http.formLogin(AbstractHttpConfigurer::disable);
 		http.authorizeHttpRequests((authorize) ->
 			authorize.requestMatchers("/**").permitAll()
+				.anyRequest().authenticated()
 		);
+		http.addFilterBefore(jwtAuthenticationFilter, ExceptionTranslationFilter.class);
 		return http.build();
 	}
 
@@ -31,4 +51,19 @@ public class SecurityConfig {
 	PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
+
+	// @Bean
+	// CorsConfigurationSource corsConfigurationSource() {
+	// 	CorsConfiguration configuration = new CorsConfiguration();
+	//
+	// 	configuration.setAllowedOrigins(List.of("http://localhost:8005"));
+	// 	configuration.setAllowedMethods(List.of("GET","POST"));
+	// 	configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
+	//
+	// 	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	//
+	// 	source.registerCorsConfiguration("/**",configuration);
+	//
+	// 	return source;
+	// }
 }
