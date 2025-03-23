a7ecd0941376545283111d5a2d8dfa5f056ab2b9
46112 <duf325@naver.com>
2025-03-23 18:23:33 +0900

    feat: 토큰 검증 필터 추가 - 토큰값을 검증하고, 검증에 통과한 경우 Authentication객체에 유저 정보를 저장 - 로그인이 필요할 경우 해당 객체 정보 여부를 통해 판별

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/global/common/exception/ErrorCode.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/global/common/exception/ErrorCode.java
@@ -8,7 +8,8 @@ public enum ErrorCode {
 	USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다"),
 	EMAIL_ALREADY_EXISTS(409, "이미 존재하는 이메일입니다"),
 	EXPIRED_TOKEN(401, "인증 토큰이 만료되었습니다"),
-	UNAUTHORIZED(401, "로그인이 필요합니다");
+	UNAUTHORIZED(401, "로그인이 필요합니다"),
+	INVALID_CREDENTIALS(401, "아이디나 비밀번호가 잘못되었습니다");
 
 	private final int status;
 	private final String message;
---
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/global/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.help.stockassistplatform.global.jwt;
+
+import java.io.IOException;
+
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.help.stockassistplatform.domain.user.entity.User;
+import com.help.stockassistplatform.domain.user.repository.UserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+	private static final String NO_CHECK_URL = "/api/auth/login";
+
+	private final JwtUtil jwtUtil;
+	private final UserRepository userRepository;
+
+	@Override
+	protected void doFilterInternal(
+		@NonNull final HttpServletRequest request,
+		@NonNull final HttpServletResponse response,
+		@NonNull final FilterChain filterChain) throws ServletException, IOException {
+		// "/api/login" 요청은 토큰 확인 x
+		if (request.getRequestURI().equals(NO_CHECK_URL)) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		checkAccessTokenAndAuthentication(request, response, filterChain);
+	}
+
+	private void checkAccessTokenAndAuthentication(final HttpServletRequest request, final HttpServletResponse response,
+		final FilterChain filterChain) throws ServletException, IOException {
+		jwtUtil.extractAccessTokenFromRequest(request)
+			.filter(jwtUtil::isTokenValidate)
+			.flatMap(jwtUtil::extractUsername)
+			.flatMap(userRepository::findByUsername)
+			.ifPresent(this::saveAuthentication);
+
+		filterChain.doFilter(request, response);
+	}
+
+	private void saveAuthentication(final User myUser) {
+		final UserDetails userDetails = CustomUser.from(myUser);
+		final Authentication authentication =
+			new UsernamePasswordAuthenticationToken(
+				userDetails,
+				userDetails.getPassword(),
+				userDetails.getAuthorities()
+			);
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+	}
+}
