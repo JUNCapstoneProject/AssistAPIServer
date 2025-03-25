63d7b5e8a677ceb372e7b081f8c2accaea34b245
46112 <duf325@naver.com>
2025-03-25 19:46:35 +0900

    fix: 유저 조회 쿼리 최적화 - 토큰 검증 시 발생하던 N+1 문제 해결 - Join Fetch 쿼리 추가

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/user/repository/UserRepository.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.help.stockassistplatform.domain.user.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.help.stockassistplatform.domain.user.entity.User;
@@ -10,6 +12,9 @@ import com.help.stockassistplatform.domain.user.entity.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 	@Transactional(readOnly = true)
 	Optional<User> findByUsername(String username);
+
+	@Query("SELECT u FROM User u JOIN FETCH u.userProfile WHERE u.username = :username")
+	Optional<User> findWithProfileByUsername(@Param("username") String username);
 
 	boolean existsByUsername(String username);
 }
