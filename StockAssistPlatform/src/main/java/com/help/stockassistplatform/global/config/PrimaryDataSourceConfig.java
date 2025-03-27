73524c49b8bccf26e484b3662009270f3dd4d95d
46112 <duf325@naver.com>
2025-03-27 14:35:57 +0900

    feat: 다중 DB설정 - 수동 JPA 설정

---
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/global/config/PrimaryDataSourceConfig.java
@@ -0,0 +1,77 @@
+package com.help.stockassistplatform.global.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import jakarta.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+	basePackages = "com.help.stockassistplatform.domain.user.repository",
+	entityManagerFactoryRef = "primaryEntityManagerFactory",
+	transactionManagerRef = "primaryTransactionManager"
+)
+public class PrimaryDataSourceConfig {
+	@Value("${spring.datasource.primary.url}")
+	private String dbUrl;
+
+	@Value("${spring.datasource.primary.username}")
+	private String dbUser;
+
+	@Value("${spring.datasource.primary.password}")
+	private String dbPassword;
+
+	@Primary
+	@Bean(name = "primaryDataSource")
+	public DataSource dataSource() {
+		final HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(dbUrl);
+		config.setUsername(dbUser);
+		config.setPassword(dbPassword);
+		config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		return new HikariDataSource(config);
+	}
+
+	@Primary
+	@Bean(name = "primaryEntityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+		EntityManagerFactoryBuilder builder,
+		@Qualifier("primaryDataSource") DataSource dataSource) {
+		final Map<String, Object> properties = new HashMap<>();
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		properties.put("hibernate.show_sql", true);
+		properties.put("hibernate.hbm2ddl.auto", "update");
+
+		return builder
+			.dataSource(dataSource)
+			.packages("com.help.stockassistplatform.domain.user.entity")
+			.persistenceUnit("primary")
+			.properties(properties)
+			.build();
+	}
+
+	@Primary
+	@Bean(name = "primaryTransactionManager")
+	public PlatformTransactionManager transactionManager(
+		@Qualifier("primaryEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+		return new JpaTransactionManager(entityManagerFactory);
+	}
+}
