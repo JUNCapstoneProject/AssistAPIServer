5c5ee035fa02f4f6639c4c134e9b3d14d48ac58b
46112 <duf325@naver.com>
2025-03-27 19:55:51 +0900

    feat: news_vw에 대응되는 entity와 repository 생성

---
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/news/entity/NewsView.java
@@ -0,0 +1,51 @@
+package com.help.stockassistplatform.domain.news.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.Immutable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "news_vw")
+@Immutable
+@Getter
+@NoArgsConstructor
+public class NewsView {
+
+	@Id
+	@Column(name = "crawling_id")
+	private String id;
+
+	@Column(name = "organization")
+	private String organization;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "hits")
+	private Long hits;
+
+	@Column(name = "author")
+	private String author;
+
+	@Column(name = "posted_at")
+	private LocalDateTime postedAt;
+
+	@Column(name = "content")
+	private String content;
+
+	@Column(name = "tags")
+	private String tags;
+
+	// @Column(name = "ai_analysis")
+	// private String aiAnalysis;
+
+	@Column(name = "url")
+	private String url;
+}
---
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/news/repository/NewsRepository.java
@@ -0,0 +1,15 @@
+package com.help.stockassistplatform.domain.news.repository;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.help.stockassistplatform.domain.news.entity.NewsView;
+
+@Repository
+public interface NewsRepository extends JpaRepository<NewsView, String> {
+	Slice<NewsView> findByTags(String tags, Pageable pageable);
+
+	Slice<NewsView> findAllBy(Pageable pageable);
+}
