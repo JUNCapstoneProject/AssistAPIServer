e93a788ad056dc953f9daa0646ef1e2a06493052
46112 <duf325@naver.com>
2025-04-02 00:19:13 +0900

    fix: DB 필드값이 수정됨에 따라 발생한 엔티티 매핑 오류 수정 - tags -> tag - url -> target_url

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/news/repository/NewsRepository.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/news/repository/NewsRepository.java
@@ -9,7 +9,7 @@ import com.help.stockassistplatform.domain.news.entity.NewsView;
 
 @Repository
 public interface NewsRepository extends JpaRepository<NewsView, String> {
-	Slice<NewsView> findByTags(String tags, Pageable pageable);
+	Slice<NewsView> findByTag(String tags, Pageable pageable);
 
 	Slice<NewsView> findAllBy(Pageable pageable);
 }
