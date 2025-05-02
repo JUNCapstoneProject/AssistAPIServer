fbd3d8ca2bbeaf60da464db1ee34fee2a4ff2149
Changyeol Choe <duf325@naver.com>
2025-05-02 23:34:55 +0900

    style: 상세 리포트 조회시 category응답값 추가

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/report/dto/response/UserReportDetailResponse.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/report/dto/response/UserReportDetailResponse.java
@@ -3,10 +3,11 @@ package com.help.stockassistplatform.domain.report.dto.response;
 import com.help.stockassistplatform.domain.report.user.entity.UserReport;
 
 public record UserReportDetailResponse(
+	String category,
 	String content,
 	boolean isAuthor
 ) {
 	public static UserReportDetailResponse of(final UserReport report, final boolean isAuthor) {
-		return new UserReportDetailResponse(report.getContent(), isAuthor);
+		return new UserReportDetailResponse(report.getCategory(), report.getContent(), isAuthor);
 	}
 }
