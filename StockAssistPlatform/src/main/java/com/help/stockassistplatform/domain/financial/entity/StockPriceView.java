838fba600c223368eb870dd5695dd8ba81fefb9f
Changyeol Choe <duf325@naver.com>
2025-05-07 16:28:00 +0900

    feat: stockView에서 시가총액 기준 top 8 ticker와 name을 가져오는 쿼리 작성

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/financial/entity/StockPriceView.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/financial/entity/StockPriceView.java
@@ -1,12 +1,16 @@
 package com.help.stockassistplatform.domain.financial.entity;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
 import org.hibernate.annotations.Immutable;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "stock_vw")
@@ -14,24 +18,24 @@ import java.time.LocalDateTime;
 @Getter
 public class StockPriceView {
 
-    @Id
-    @Column(name = "crawling_id")
-    private String crawlingId;
+	@Id
+	@Column(name = "crawling_id")
+	private String crawlingId;
 
-    @Column(name = "posted_at") // ← 실제 DB 컬럼 이름이 이거일 가능성 높음. 확인 필요!
-    private LocalDateTime postedAt;
+	@Column(name = "posted_at") // ← 실제 DB 컬럼 이름이 이거일 가능성 높음. 확인 필요!
+	private LocalDateTime postedAt;
 
-    @Column(name = "name_kr")
-    private String name;
+	@Column(name = "name_kr")
+	private String name;
 
-    private String ticker;
+	private String ticker;
 
+	private BigDecimal open;
+	private BigDecimal high;
+	private BigDecimal low;
+	private BigDecimal close;
+	private Long volume;
 
-    private BigDecimal open;
-    private BigDecimal high;
-    private BigDecimal low;
-    private BigDecimal close;
-    private Long volume;
-
-    private Float change;
+	private Float change;
+	private BigInteger marketCap;
 }
