3b0f3c47cdfb3607a80710146502a0694614ee9b
Changyeol Choe <duf325@naver.com>
2025-05-12 22:08:14 +0900

    feat: 비밀번호 변경 API 컨트롤러에 추가

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/user/controller/AuthController.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/user/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.help.stockassistplatform.domain.user.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,9 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.help.stockassistplatform.domain.user.dto.request.LoginRequestDto;
+import com.help.stockassistplatform.domain.user.dto.request.PasswordResetConfirmDto;
+import com.help.stockassistplatform.domain.user.dto.request.PasswordResetRequestDto;
 import com.help.stockassistplatform.domain.user.dto.response.LoginCheckResponseDto;
 import com.help.stockassistplatform.domain.user.service.AuthService;
+import com.help.stockassistplatform.domain.user.service.EmailVerificationService;
+import com.help.stockassistplatform.domain.user.service.UserService;
+import com.help.stockassistplatform.domain.user.service.VerificationTokenService;
 import com.help.stockassistplatform.global.common.response.ApiResponse;
+import com.help.stockassistplatform.global.jwt.CustomUser;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -23,6 +31,10 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class AuthController {
 	private final AuthService authService;
+	private final UserService userService;
+
+	private final VerificationTokenService tokenService;
+	private final EmailVerificationService emailService;
 
 	// 로그인
 	@PostMapping("/login")
@@ -54,5 +66,29 @@ public class AuthController {
 	public ApiResponse<?> loginCheck(final HttpServletRequest request) {
 		final Boolean isLogin = authService.checkLogin(request);
 		return ApiResponse.success(new LoginCheckResponseDto(isLogin));
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("/password-reset-request")
+	public ApiResponse<?> requestPasswordReset(@Valid @RequestBody final PasswordResetRequestDto resetRequestDto,
+		final HttpServletRequest request) {
+		final String email = resetRequestDto.email();
+
+		final String token = tokenService.createToken();
+		tokenService.saveResetEmailToRedis(token, email);
+		emailService.sendPasswordResetEmail(token, email);
+		return ApiResponse.success(null);
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("/password-reset")
+	public ApiResponse<?> confirmPasswordReset(
+		@AuthenticationPrincipal final CustomUser userDetail,
+		@Valid @RequestBody final PasswordResetConfirmDto request
+	) {
+		final String email = tokenService.getResetEmailFromRedis(request.token());
+		userService.checkEmailOwnership(userDetail, email);
+		userService.changeUserPassword(userDetail, request.newPassword());
+		return ApiResponse.success(null);
 	}
 }
