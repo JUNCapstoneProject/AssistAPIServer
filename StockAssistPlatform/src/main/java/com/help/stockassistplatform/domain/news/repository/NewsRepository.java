270f3bcf26918e33dcbf22ec049cc1c3f662293f
Changyeol Choe <duf325@naver.com>
2025-05-22 23:06:04 +0900

    feat: findByTagInTagsWithAnalysis 쿼리 추가

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/news/repository/NewsRepository.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/news/repository/NewsRepository.java
@@ -3,13 +3,16 @@ package com.help.stockassistplatform.domain.news.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.help.stockassistplatform.domain.news.entity.NewsView;
 
 @Repository
 public interface NewsRepository extends JpaRepository<NewsView, String> {
-	Slice<NewsView> findByTagsWithAnalysis(String tags, Pageable pageable);
+	@Query("SELECT n FROM NewsView n WHERE n.tagsWithAnalysis LIKE CONCAT('%', :tag, ':%')")
+	Slice<NewsView> findByTagInTagsWithAnalysis(@Param("tag") String tag, Pageable pageable);
 
 	Slice<NewsView> findAllBy(Pageable pageable);
 }
