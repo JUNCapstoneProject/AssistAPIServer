517b862057a43f98c123af5ad7c37797b56bcef8
Changyeol Choe <duf325@naver.com>
2025-05-22 22:06:13 +0900

    fix: StockSummaryDto Json 직렬화를 위해 Record로 변경

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/stock/dto/response/StockSummaryDto.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/stock/dto/response/StockSummaryDto.java
@@ -5,30 +5,28 @@ import java.math.BigInteger;
 
 import com.help.stockassistplatform.domain.financial.entity.StockPriceView;
 
-public class StockSummaryDto {
-	private final String name;
-	private final String symbol;
-	private final BigInteger marketCap;
-	private final BigDecimal price;
-	private final Float change;
-	private final Long volume;
-	private final BigDecimal pe;
-	private final BigDecimal eps;
-	private final BigDecimal dividend;
-
-	private StockSummaryDto(final StockPriceView view) {
-		this.name = view.getName();
-		this.symbol = view.getTicker();
-		this.marketCap = view.getMarketCap();
-		this.price = view.getClose();
-		this.change = view.getChange();
-		this.volume = view.getVolume();
-		this.pe = view.getPer();
-		this.eps = view.getEps();
-		this.dividend = view.getDividend();
-	}
-
+public record StockSummaryDto(
+	String name,
+	String symbol,
+	BigInteger marketCap,
+	BigDecimal price,
+	Float change,
+	Long volume,
+	BigDecimal pe,
+	BigDecimal eps,
+	BigDecimal dividend
+) {
 	public static StockSummaryDto from(final StockPriceView view) {
-		return new StockSummaryDto(view);
+		return new StockSummaryDto(
+			view.getName(),
+			view.getTicker(),
+			view.getMarketCap(),
+			view.getClose(),
+			view.getChange(),
+			view.getVolume(),
+			view.getPer(),
+			view.getEps(),
+			view.getDividend()
+		);
 	}
 }
