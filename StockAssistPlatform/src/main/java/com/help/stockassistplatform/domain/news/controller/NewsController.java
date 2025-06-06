b33333d087f17f63910075b3610db9224f432fd8
송현민 <157588023+wildyoung@users.noreply.github.com>
2025-06-07 00:25:50 +0900

    Update NewsController.java

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/news/controller/NewsController.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/news/controller/NewsController.java
@@ -2,6 +2,9 @@ package com.help.stockassistplatform.domain.news.controller;
 
 import static org.springframework.data.domain.Sort.Direction.*;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
