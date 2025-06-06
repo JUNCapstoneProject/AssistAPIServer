6f929e64dc22f750c738ac3a2c777497aa678219
송현민 <157588023+wildyoung@users.noreply.github.com>
2025-06-07 00:29:35 +0900

    Update NewsRepository.java

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/news/repository/NewsRepository.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/news/repository/NewsRepository.java
@@ -1,5 +1,7 @@
 package com.help.stockassistplatform.domain.news.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,11 +11,16 @@ import com.help.stockassistplatform.domain.news.entity.NewsView;
 
 @Repository
 public interface NewsRepository extends JpaRepository<NewsView, String> {
+
 	Slice<NewsView> findByTag(String tag, Pageable pageable);
+	Slice<NewsView> findByTagAndAiAnalysis(String tag, Integer aiAnalysis, Pageable pageable);
 
-	Slice<NewsView> findAllBy(Pageable pageable);
+	Slice<NewsView> findByTagIn(List<String> tags, Pageable pageable);
+	Slice<NewsView> findByTagInAndAiAnalysis(List<String> tags, Integer aiAnalysis, Pageable pageable); // ← 이거 꼭 추가
 
+	// 감성 필터만
 	Slice<NewsView> findByAiAnalysis(Integer aiAnalysis, Pageable pageable);
 
-	Slice<NewsView> findByTagAndAiAnalysis(String tag, Integer aiAnalysis, Pageable pageable);
+	// 전체 조회
+	Slice<NewsView> findAllBy(Pageable pageable);
 }
