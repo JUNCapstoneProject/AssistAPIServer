a25b5f81a28fb76fa58d8d3ca859b9527b614c8d
Changyeol Choe <duf325@naver.com>
2025-06-08 00:15:46 +0900

    feat: 찜하기 도중 티커 데이터 조회 불가 예외 처리

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/wishlist/service/WishlistService.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/wishlist/service/WishlistService.java
@@ -6,12 +6,15 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.help.stockassistplatform.domain.financial.repository.StockPriceViewRepository;
 import com.help.stockassistplatform.domain.stock.service.StockQueryService;
 import com.help.stockassistplatform.domain.user.entity.User;
 import com.help.stockassistplatform.domain.wishlist.dto.WishlistItemDto;
 import com.help.stockassistplatform.domain.wishlist.entity.UserWishlist;
 import com.help.stockassistplatform.domain.wishlist.entity.UserWishlistId;
 import com.help.stockassistplatform.domain.wishlist.repository.UserWishlistRepository;
+import com.help.stockassistplatform.global.common.exception.CustomException;
+import com.help.stockassistplatform.global.common.exception.ErrorCode;
 import com.help.stockassistplatform.global.jwt.CustomUser;
 
 import lombok.RequiredArgsConstructor;
@@ -22,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 public class WishlistService {
 
 	private final UserWishlistRepository wishlistRepository;
+	private final StockPriceViewRepository stockPriceViewRepository;
 	private final StockQueryService stockQueryService;
 
 	@Transactional
@@ -30,6 +34,9 @@ public class WishlistService {
 		final UserWishlistId id = new UserWishlistId(user.getUserId(), ticker);
 		if (wishlistRepository.existsById(id))
 			return;
+		if (!stockPriceViewRepository.existsById(ticker)) {
+			throw new CustomException(ErrorCode.HEART_TICKER_NOT_FOUND);
+		}
 		wishlistRepository.save(UserWishlist.of(user, ticker));
 	}
 
