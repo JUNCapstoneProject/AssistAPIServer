fe3191b58ba2edc7f17691282ed3773f8d0bc0c3
Changyeol Choe <duf325@naver.com>
2025-06-08 00:08:30 +0900

    fix: JoinColumn으로 유저 FK 명시화

--- StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/wishlist/entity/UserWishlist.java
+++ StockAssistPlatform/src/main/java/com/help/stockassistplatform/domain/wishlist/entity/UserWishlist.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
@@ -31,6 +32,7 @@ public class UserWishlist {
 
 	@MapsId("userId")
 	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 
 	@Column(name = "added_at", nullable = false)
